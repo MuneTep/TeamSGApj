a41a689177eec36ee9f955dfbf43e897861cd048
Ghuitb <Ghuitb@user.noreply.github.com>
2024-07-21 14:13:42 +0900

    [Update] 충돌 이벤트처리 가능한 구체 추가

---
+++ Source/SGA_Teamprj/Private/CollisionBox.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CollisionBox.h"
+#include "Components/SphereComponent.h"
+
+// Sets default values
+ACollisionBox::ACollisionBox()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CollisionMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("DefaultMeshComponent"));
+	RootComponent = CollisionMesh;
+	Sphere = CreateDefaultSubobject<USphereComponent>(TEXT("DefaultSphere"));
+	Sphere->SetupAttachment(GetRootComponent());
+}
+
+// Called when the game starts or when spawned
+void ACollisionBox::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	Sphere->OnComponentBeginOverlap.AddDynamic(this, &ACollisionBox::CollisionBoxOn);
+	Sphere->OnComponentEndOverlap.AddDynamic(this, &ACollisionBox::CollisionBoxOff);
+}
+
+void ACollisionBox::CollisionBoxOn(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	GEngine->AddOnScreenDebugMessage(1, 30.f, FColor::Blue, TEXT("충돌!"));
+}
+
+void ACollisionBox::CollisionBoxOff(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	GEngine->AddOnScreenDebugMessage(1, 30.f, FColor::Red, TEXT("충돌 해제"));
+}
+
+// Called every frame
+void ACollisionBox::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/SGA_Teamprj/Public/CollisionBox.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CollisionBox.generated.h"
+
+class USphereComponent;
+UCLASS()
+class SGA_TEAMPRJ_API ACollisionBox : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACollisionBox();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	UFUNCTION()
+	void CollisionBoxOn(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	UFUNCTION()
+	void CollisionBoxOff(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* CollisionMesh;
+	UPROPERTY(VisibleAnywhere)
+	USphereComponent* Sphere;
+
+};
